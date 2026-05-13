61953a85ee03124cc23f006758ac933dbc864fe7
NIKOLOZ SIORDIA <235945203+siordianikoloz-sys@users.noreply.github.com>
2026-05-13 22:07:56 +0200

    Update Header.jsx

--- Header.jsx
+++ Header.jsx
@@ -1,1 +1,16 @@
 
+
+import React from "react"
+import { Link } from "react-router-dom"
+
+export default function Header(){
+    return(
+        <header>
+            <h1>Element Personality</h1>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/quiz">Quiz</Link>
+            </nav>
+        </header>
+    )
+}
