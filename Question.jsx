6cb821a382b2a20d498220372a6878fb1c64485f
NIKOLOZ SIORDIA <235945203+siordianikoloz-sys@users.noreply.github.com>
2026-05-13 22:08:20 +0200

    Update Question.jsx

--- Question.jsx
+++ Question.jsx
@@ -1,1 +1,51 @@
 
+import React from "react";
+
+export default function Question({ question, options, onAnswer }) {
+    useEffect(
+  function () {
+    if (currentQuestionIndex === questions.length) {
+      const selectedElement = determineElement(answers);
+      setElement(selectedElement);
+      fetchArtwork(keywords[selectedElement]);
+    }
+  },
+  [currentQuestionIndex]
+);
+  return (
+    <div>
+      <h2>{question}</h2>
+      {options.map(function (option) {
+        return (
+          <button
+            key={option}
+            onClick={function () {
+              onAnswer(option);
+            }}
+          >
+            {option}
+          </button>
+        );
+      })}
+    </div>
+  );
+function handleAnswer(answer) {
+  setAnswers([...answers, answer]);
+  setCurrentQuestionIndex(currentQuestionIndex + 1);
+};
+
+function handleUserFormSubmit(name) {
+  setUserName(name);
+};
+
+function determineElement(answers) {
+  const counts = {};
+  answers.forEach(function(answer) {
+    const element = elements[answer];
+    counts[element] = (counts[element] || 0) + 1;
+  });
+  return Object.keys(counts).reduce(function(a, b) {
+    return counts[a] > counts[b] ? a : b
+  });
+};
+}
